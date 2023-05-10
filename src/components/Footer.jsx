510582b760ce6625aeab17e1987539d3ad7e8db0
carlosBohorquuez20 <carlos209504@gmail.com>
2023-05-10 00:40:02 -0500

    second change

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 const Footer = () => {
   return (
     <footer>
-      <p>Ludy baina & Carlos Bohorquez Parra</p>
+      <p>Carlos Bohorquez Parra</p>
       <div className='icons-footer'>
       <i className="fa-brands fa-instagram"></i>
       <i className="fa-brands fa-youtube"></i>
